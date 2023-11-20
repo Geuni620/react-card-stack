c0cd26ab412cbef2786e90d7df3496af5f418d22
이근휘 <dlrmsgnl620@naver.com>
2023-11-20 16:59:57 +0900

    click 했을 때 가장 뒤면으로

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import { useState } from 'react';
 
 function App() {
-  const [hoveredItemId, setHoveredItemId] = useState<null | number>(null);
+  const [items, setItems] = useState(MockingItemData);
+
+  const handleClick = (clickedId: number) => {
+    const newItems = [...items];
+    const clickedIndex = newItems.findIndex((item) => item.id === clickedId);
+    const clickedItem = newItems.splice(clickedIndex, 1)[0];
+    newItems.push(clickedItem); // 클릭된 아이템을 배열의 끝으로 이동
+    setItems(newItems);
+  };
 
   return (
     <div className="scene">
       <div className="carousel">
-        {MockingItemData.map(({ id }, index) => {
-          const isHovered = hoveredItemId === id;
-          const topValue = isHovered
-            ? `${50 - 80 * index - 30}px`
-            : `${200 - 80 * index}px`;
+        {items.map(({ id }, index) => {
+          const topValue = `${200 - 80 * index}px`;
           const rotateYValue = `${20 - 5 * index}deg`;
           const translateZValue = `${500 - 300 * index}px`;
 
           return (
             <div
+              key={id}
               className="item"
-              onMouseEnter={() => setHoveredItemId(id)}
-              onMouseLeave={() => setHoveredItemId(null)}
+              onClick={() => handleClick(id)}
               style={{
                 top: topValue,
                 backgroundColor: id % 2 === 0 ? '#ff7979' : '#bfa75b',
@@ -62,6 +67,6 @@ const MockingItemData = [
     id: 8,
   },
   {
-    id: 19,
+    id: 9,
   },
 ];
